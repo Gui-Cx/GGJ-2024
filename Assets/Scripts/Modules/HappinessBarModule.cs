ce8c580bc086b8d16d8fd18ff75aaaaf6c5682fb
BiscuitPrime <henri.nomico@gmail.com>
2024-01-27 10:29:43 +0100

    Added working happiness bar setup

---
+++ Assets/Scripts/Modules/HappinessBarModule.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Script contained by the happiness bar object, to be modified by the NPC scripts (NPCHappinessBarController)
+/// </summary>
+public class HappinessBarModule : MonoBehaviour
+{
+    private Slider _slider;
+
+    private void Awake()
+    {
+        _slider = GetComponent<Slider>();
+    }
+    public void SetMaxHappiness(int val)
+    {
+        _slider.maxValue = val;
+        _slider.value = val;
+    }
+
+    public void SetHappinessValue(int val)
+    {
+        _slider.value = val;
+    }
+}
--- Assets/Scripts/NPC/NPCHappinessBarController.cs
+++ Assets/Scripts/NPC/NPCHappinessBarController.cs
@@ -1,20 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 /// <summary>
 /// This script will handle the ever going down bar
 /// </summary>
 public class NPCHappinessBarController : MonoBehaviour
 {
-    [SerializeField] private int _maxLevel;
+    [Header("Happiness Bar values")]
+    [SerializeField] private int _maxLevel = 500;
     [SerializeField] private int _curLevel;
 
+    [Header("Timer values")]
     [SerializeField] private float _barDownUpdateTimer=1f;
+
+    [Header("Happiness Bar elements")]
+    [SerializeField] private GameObject _happinessBar;
+    private HappinessBarModule _barModule;
+
+    private void OnValidate()
+    {
+        Assert.IsNotNull(_happinessBar);
+    }
 
     private void Start()
     {
+        _barModule = _happinessBar.GetComponent<HappinessBarModule>();
+        _curLevel = _maxLevel;
+        _barModule.SetMaxHappiness(_maxLevel);
         StartCoroutine(BarDownUpdate());
+    }
+
+    private void Update()
+    {
+        Debug.Log("NPC : " + gameObject.name + " : Happiness Bar : " + _curLevel);
     }
 
     /// <summary>
@@ -29,5 +49,14 @@ public class NPCHappinessBarController : MonoBehaviour
         {
             _curLevel = 0;
         }
+        UpdateVisualHappinessBar();
+        StartCoroutine(BarDownUpdate());
     }
+
+    private void UpdateVisualHappinessBar()
+    {
+        _barModule.SetHappinessValue(_curLevel);
+    }
+
+
 }
